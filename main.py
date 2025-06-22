fb9f22a18e23b2b56495a9ba5a41eddab260a7c5
Guilherme Sartori <sartori.guilherme@gmail.com>
2025-06-22 11:43:49 -0300

    Adicionado o rascunho main

---
+++ main.py
@@ -0,0 +1,12 @@
+# numero = int(input("insira um numero: "))
+# if isinstance (numero,):
+#     print("A variável é um inteiro")
+# else:
+#         print("A variável não é um inteiro.")
+
+idade = 22
+if idade < 18:
+    print("não pode dirigir")
+else:
+    print("Pode dirigir")
+    
